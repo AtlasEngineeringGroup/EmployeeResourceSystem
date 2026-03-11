46414460fb91df6d6dfb5a18b7ce5df7db0cad66
MrJoaoAbe <joaovictorsouzaabe@gmail.com>
2026-03-11 11:49:49 -0300

    Add-Method-CadastrarColaborador

--- src/SistemaERS.java
+++ src/SistemaERS.java
@@ -1,25 +1,20 @@
 import java.util.ArrayList;
 public class SistemaERS {
+
+    ArrayList<Colaborador> listaColaboradores = new ArrayList<>();
+    ArrayList<Recurso> listaRecursos = new ArrayList<>();
+    ArrayList<Alocacao> listaAlocacoes = new ArrayList<>();
+
+    public void cadastrarColaborador(int id, String nome, String cargo, double salario, String dataDeAdmissao) {
+        Colaborador novoColaborador = new Colaborador(id, nome, cargo, salario, dataDeAdmissao);
+        listaColaboradores.add(novoColaborador);
+        System.out.println("Colaborador " + nome + " cadastrado com sucesso!");
+    }
+
+
     public static void main(String[] args) {
+        SistemaERS sistema = new SistemaERS();
 
-        Colaborador colaborador1 = new Colaborador(1,"Joao","Desenvolvedor", 15000, "01/6/2020");
-        Colaborador colaborador2 = new Colaborador(1,"Carlos","Desenvolvedor", 20000, "06/2/2017");
-        Colaborador colaborador3 = new Colaborador(1,"Rodrigo","Estagiário", 2000, "10/2/2026");
-
-        ArrayList<String> listaColaboladores = new ArrayList<>();
-
-        listaColaboladores.add(colaborador1.nome);
-        listaColaboladores.add(colaborador2.nome);
-        listaColaboladores.add(colaborador3.nome);
-
-
-        Recurso recurso1 = new Recurso(1,"Computador","Aparelho",true,10000);
-        Recurso recurso2 = new Recurso(2,"LapTop","Aparelho",true,8000);
-
-        ArrayList<String> listaRecursos = new ArrayList<>();
-
-        listaColaboladores.add(recurso1.nomeDoRecurso);
-        listaColaboladores.add(recurso2.nomeDoRecurso);
-
+        sistema.cadastrarColaborador(1, "João", "Dev", 5000, "07/09/2022");
     }
 }

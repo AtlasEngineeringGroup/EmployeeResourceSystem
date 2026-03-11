662d392db4c732cf0f96f47724a27c488f49f400
MrJoaoAbe <joaovictorsouzaabe@gmail.com>
2026-03-11 11:57:11 -0300

    Add-Method-CadastrarColaborador

--- src/SistemaERS.java
+++ src/SistemaERS.java
@@ -11,10 +11,17 @@ public class SistemaERS {
         System.out.println("Colaborador " + nome + " cadastrado com sucesso!");
     }
 
+    public void cadastrarRecuros(int id, String nomeDoRecurso, String categoria, boolean disponivel, double valorEstimado){
+        Recurso novoRecurso = new Recurso(id, nomeDoRecurso, categoria, disponivel, valorEstimado);
+        listaRecursos.add(novoRecurso);
+        System.out.println("Recurso " + nomeDoRecurso + " cadastrado com sucesso!");
+    }
+
 
     public static void main(String[] args) {
         SistemaERS sistema = new SistemaERS();
 
         sistema.cadastrarColaborador(1, "João", "Dev", 5000, "07/09/2022");
+        sistema.cadastrarRecuros(1,"Notebook","Eletronico",true,10000);
     }
 }
